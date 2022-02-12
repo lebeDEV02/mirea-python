0a7c6f0976912ab237262684801b5b2d7164176a
ledeDEV02 <vladimirlebedev00010@gmail.com>
2022-02-12 07:19:50 +0300

    completed second-task

--- first/second-task.py
+++ first/second-task.py
@@ -1,14 +1,14 @@
 import math
+
 x = float(input("X = "))
 accuracy = float(input("e is "))
-s=(math.pi/2)
-t=0
-k=1
+s = currentTerm = float((math.pi/2))
+t = float(0)
+k = int(0)
 
-
-while math.fabs(t) > accuracy:
-    t += (math.factorial(2*k)*math.pow(x, 2*k+1))/(math.pow(2, 2*k) * math.pow(math.factorial(k), 2) * (2*k+1))
-    k+=1
-    s -= t
-    print(math.radians(s))
-    print(math.radians(math.acos(x)))
+while math.fabs(currentTerm) > accuracy:
+    item = (math.factorial(2*k)*math.pow(x, 2*k+1))/(math.pow(2, 2*k) * math.pow(math.factorial(k), 2) * (2*k+1))
+    s -= item
+    k += 1
+    print("Приближенное значение = ", s)
+    print("Точное значение = ", math.acos(x))
